87998f70358151484900c4e755ee72dd6a11ea81
morgadodesarrollador <morgadodesarrollador@gmail.com>
2022-12-09 20:04:08 +0100

    Eliminar Modulos

--- api/src/modulos/alumnos/alumnos.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AlumnosService } from './alumnos.service';
-import { AlumnosController } from './alumnos.controller';
-
-@Module({
-  controllers: [AlumnosController],
-  providers: [AlumnosService]
-})
-export class AlumnosModule {}
--- api/src/modulos/alumnos/dto/update-alumno.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAlumnoDto } from './create-alumno.dto';
-
-export class UpdateAlumnoDto extends PartialType(CreateAlumnoDto) {}
