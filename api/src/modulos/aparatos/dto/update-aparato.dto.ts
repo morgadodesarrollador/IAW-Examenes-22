9b5627e5cfc50cad46e0a133aadb9f73b685bb2e
morgadodesarrollador <morgadodesarrollador@gmail.com>
2022-12-15 09:12:30 +0100

    seed data Incidencias

--- api/src/modulos/aparatos/dto/update-aparato.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAparatoDto } from './create-aparato.dto';
-
-export class UpdateAparatoDto extends PartialType(CreateAparatoDto) {}
--- api/src/modulos/index.ts
+++
@@ -1,2 +0,0 @@
-
-export { AuthModule } from './auth/auth.module';
